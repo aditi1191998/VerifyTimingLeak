98778b3c357a1f35502e61d2652a44b75bd5cace
Aditi Singh <aditirsingh9@gmail.com>
2021-08-13 22:52:57 +0530

    Add files via upload

--- assembly_parser.cpp
+++ assembly_parser.cpp
@@ -3,64 +3,70 @@
 #include <regex>
 #include <iterator>
 #include <fstream>
+#define testcase 200
 
 using namespace std;
 
 int main (int argc, char **argv) {
-  fstream inputText;
-  ofstream outputText;
+  fstream inputText[testcase];
+  ofstream outputText[testcase];
   string line;
   string line1;
-  string filename = argv[1];
+  string strIn;
+  string strOut;
   //string lw = "(lw)(.[^,]*)(\\d+)(,)(\\d+)(.*)";
   //string sw = "(sw)(.[^,]*)(\\d+)(,)(\\d+)(.*)";
   long posi,pos;
   string latest,latest1;
   int count = 0;
-  inputText.open(filename);
-  cout << filename << endl;
-  outputText.open("out.txt");
-  if(inputText.good()) {
-    while (getline(inputText, line)) {
+  regex re("(.*)(lw)(.[^,]*)(\\d+)(, )(\\d+)(.*)");
+  regex re1("(.*)(sw)(.[^,]*)(\\d+)(, )(\\d+)(.*)");
+  regex re2("(.*)(lb)(.[^,]*)(\\d+)(, )(\\d+)(.*)");
+  smatch match;
+  for(int i=0;i<testcase;i++) {
+  count = 0;
+  strIn = "output" + to_string(i);
+  strOut = "out" + to_string(i);
+  inputText[i].open(strIn);
+  outputText[i].open(strOut);
+  if(inputText[i].good()) {
+    while (getline(inputText[i], line)) {
       if(line.length()==0)
           continue;
       //regex re("(.*)(lw)(.[^,]*)(\\d+)(, )(\\d+)(.[^,]*)(\\d+)(.*)");
       //regex re1("(.*)(sw)(.[^,]*)(\\d+)(, )(\\d+)(.[^,]*)(\\d+)(.*)");
-      regex re("(.*)(lw)(.[^,]*)(\\d+)(, )(\\d+)(.*)");
-      regex re1("(.*)(sw)(.[^,]*)(\\d+)(, )(\\d+)(.*)");
-      regex re2("(.*)(lb)(.[^,]*)(\\d+)(, )(\\d+)(.*)");
-      smatch match;
     if (regex_search(line, match, re) == true) {
-        outputText << "{INSTR_LOAD, ";
-        outputText << stoi(match.str(6));
+        outputText[i] << "{INSTR_LOAD, ";
+        outputText[i] << stoi(match.str(6));
        // outputText << stoi(match.str(6)) +  stoi(match.str(8));
-        outputText << ", 0, 0},";
-        outputText << std::endl;
+        outputText[i] << ", 0, 0},";
+        outputText[i] << std::endl;
         count++;
     }
     else if (regex_search(line, match, re1) == true) {
-        outputText << "{INSTR_STORE, ";
-        outputText << stoi(match.str(6));
+        outputText[i] << "{INSTR_STORE, ";
+        outputText[i] << stoi(match.str(6));
         //outputText << stoi(match.str(6)) +  stoi(match.str(8));
-        outputText << ", 0, 0},";
-        outputText << std::endl;
+        outputText[i] << ", 0, 0},";
+        outputText[i] << std::endl;
         count++;
     }
     if (regex_search(line, match, re2) == true) {
-        outputText << "{INSTR_LOAD, ";
-        outputText << stoi(match.str(6));
+        outputText[i] << "{INSTR_LOAD, ";
+        outputText[i] << stoi(match.str(6));
        // outputText << stoi(match.str(6)) +  stoi(match.str(8));
-        outputText << ", 0, 0},";
-        outputText << std::endl;
+        outputText[i] << ", 0, 0},";
+        outputText[i] << std::endl;
         count++;
     }
     else {
       continue;
     }
   }
+  inputText[i].close();
+  outputText[i].close();
 }
-inputText.close();
-outputText.close();
+}
 system("g++ -o parser test.cpp");
 string str = "./parser " + to_string(count);
 const char * c = str.c_str();
---
+++ query.q
@@ -0,0 +1,1 @@
+A[] Test0a.Terminated imply Test0a.runtime>=12800
